e690aaf5518c57743924d8ac22067a9f0b156e8c
Gabriele Musco <emaildigabry@gmail.com>
2019-07-21 10:27:39 +0200

    readme, license, xml parser

---
+++ README.md
@@ -0,0 +1,3 @@
+# GNOME Feeds
+
+Soon to be RSS reader for GNOME. GPL3 License.
---
+++ gnome-feeds/xml_parser.py
@@ -0,0 +1,82 @@
+from lxml import etree
+from email.utils import parsedate_to_datetime
+from gettext import gettext as _
+
+"""
+quick XML parsing notes
+
+<?xml version="1.0"?>               IGNORE THIS
+<rss version="2.0">                 THIS IS THE ROOT
+    <channel>                       THIS IS ROOT'S 0th AND ONLY CHILD
+        <title>Phoronix</title>
+        <link>https://www.phoronix.com/</link>
+        <description>Linux Hardware Reviews &amp; News</description>
+        <language>en-us</language>
+        <item>
+            <title>RadeonSI Gallium3D Driver Adds Navi Wave32 Support</title>
+            <link>http://www.phoronix.com/scan.php?page=news_item&amp;px=RadeonSI-Wave32-Lands</link>
+            <guid>http://www.phoronix.com/scan.php?page=news_item&amp;px=RadeonSI-Wave32-Lands</guid>
+            <description>One of the new features to the RDNA architecture with Navi is support for single cycle issue Wave32 execution on SIMD32. Up to now the RadeonSI code was using just Wave64 but now there is support in this AMD open-source Linux OpenGL driver for Wave32...</description>
+            <pubDate>Sat, 20 Jul 2019 07:19:33 -0400</pubDate>
+        </item>
+        <item>
+        ...
+"""
+
+class FeedItem:
+    def __init__(self, item, parent_feed):
+        self.title = ''
+        self.link = ''
+        self.guid = ''
+        self.description = ''
+        self.pub_date = None
+        self.parent_feed = parent_feed
+
+        for el in item:
+            tag = el.tag.lower()
+            if tag == 'title':
+                self.title = el.text
+            elif tag == 'link':
+                self.link = el.text
+            elif tag == 'guid':
+                self.guid = el.text
+            elif tag == 'description':
+                self.description = el.text
+            elif tag == 'pubdate': # should be 'pubDate' but it's lower()
+                self.pub_date = parsedate_to_datetime(el.text)
+            else:
+                print(_('WARNING: unrecognized tag {0}').format(el.tag))
+
+    def __repr__(self):
+        return f'FeedItem Object `{self.title}` from Feed {self.parent_feed.title}'
+
+class Feed:
+    def __init__(self, feedpath):
+        with open(feedpath, 'r') as fd:
+            tree = etree.parse(fd)
+            fd.close()
+        root = tree.getroot()
+        
+        self.title = ''
+        self.link = ''
+        self.description = ''
+        self.language = ''
+        self.items = []
+
+        for el in root[0]: # root[0] should be channel
+            tag = el.tag.lower()
+            if tag == 'title':
+                self.title = el.text
+            elif tag == 'link':
+                self.link = el.text
+            elif tag == 'description':
+                self.description = el.text
+            elif tag == 'language':
+                self.language = el.text
+            elif tag == 'item':
+                self.items.append(FeedItem(el, self))
+            else:
+                print(_('WARNING: unrecognized tag {0}').format(el.tag))
+
+    def __repr__(self):
+        return f'Feed Object `{self.title}`; {len(self.items)} items'
