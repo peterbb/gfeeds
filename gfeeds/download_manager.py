cfbfaf49df7d873864b8db0ae17dca135d33b35c
Gabriele Musco <emaildigabry@gmail.com>
2019-08-10 12:34:38 +0200

    removed unnecessary cache hit check on feed download

--- gfeeds/download_manager.py
+++ gfeeds/download_manager.py
@@ -28,21 +28,9 @@ def download_feed(link):
     req = requests.get(link)
     if req.status_code == 200:
         #print(_('Download of `{0}` successful').format(link))
-        needs_save = False
-        if dest_path.is_file():
-            with open(dest_path, 'r') as fd:
-                if shasum(req.text) == shasum(fd.read()):
-                    pass
-                    #print(_('Hit cache for `{0}`, skipping file update').format(link))
-                else:
-                    needs_save = True
-                fd.close()
-        else:
-            needs_save = True
-        if needs_save:
-            with open(dest_path, 'w') as fd:
-                fd.write(req.text)
-                fd.close()
+        with open(dest_path, 'w') as fd:
+            fd.write(req.text)
+            fd.close()
         return (dest_path, link)
     else:
         print(_('Error downloading `{0}`, code `{1}`').format(link, req.status_code))
