8311703f8790fbf29cd0c7dc1c8ad86e9eabc29b
Gabriele Musco <gabmus@disroot.org>
2020-01-07 23:32:07 +0100

    removed debug prints

--- gfeeds/reader_mode_style.py
+++ gfeeds/reader_mode_style.py
@@ -8,6 +8,3 @@ DARK_MODE_CSS = Gio.resources_lookup_data(
     '/org/gabmus/gfeeds/reader_mode_dark_style.css',
     Gio.ResourceLookupFlags.NONE
 ).get_data().decode()
-
-print('CSS HERE!\n'*100)
-print(CSS)
