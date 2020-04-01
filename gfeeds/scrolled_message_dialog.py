304f68a38ad13ea98262a2c0f4112b4c49153027
Gabriele Musco <gabmus@disroot.org>
2020-04-01 20:48:22 +0200

    made scrolled message dialog title pretty again

--- gfeeds/scrolled_message_dialog.py
+++ gfeeds/scrolled_message_dialog.py
@@ -13,7 +13,14 @@ class ScrolledMessageDialog(Gtk.MessageDialog):
         self.get_content_area().pack_start(self.scrolled_win, True, True, 0)
         self.get_content_area().set_spacing(0)
         self.get_message_area().set_spacing(0)
-        self.get_message_area().get_children()[0].set_margin_bottom(20)
+        self.title_label = self.get_message_area().get_children()[0]
+        self.secondary_label = self.get_message_area().get_children()[1]
+        self.title_label.set_margin_bottom(20)
+        # Hiding default secondary text, because I need it set to make the
+        # title pretty but I use a custom widget for the secondary text
+        self.secondary_label.set_visible(False)
+        self.secondary_label.set_no_show_all(True)
 
     def format_secondary_markup(self, message):
+        super().format_secondary_markup(' ')
         self.label.set_markup(message)
