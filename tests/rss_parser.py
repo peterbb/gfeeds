e3438e3ab023c54debb0315a4e2bc8c496c999d8
Gabriele Musco <gabmus@disroot.org>
2020-05-10 17:09:03 +0200

    added partial feed creation test

--- tests/rss_parser.py
+++ tests/rss_parser.py
@@ -1,9 +1,10 @@
 import unittest
-from unittest.mock import Mock, MagicMock, patch
+from unittest.mock import Mock, MagicMock, patch, mock_open
 from parameterized import parameterized
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
+from .sample_rss import SAMPLE_RSS
 
-from gfeeds.rss_parser import FeedItem, FakeFeed
+from gfeeds.rss_parser import FeedItem, FakeFeed, Feed
 
 
 class TestFeedItem(unittest.TestCase):
@@ -48,6 +49,43 @@ class TestFeedItem(unittest.TestCase):
             datetime.now(timezone.utc) - feeditem.pub_date
         ).seconds.should.be.lower_than(1)
 
-    # @patch('gfeeds.rss_parser.ConfManager')
-    # def test_Feed_create(self, ConfManager_mock):
-    #     pass
+    # TODO: test with atom feed, feeds with non utf encodings
+    @patch(
+            'builtins.open',
+            new_callable=mock_open,
+            read_data=SAMPLE_RSS.encode()  # wants bytes
+    )
+    @patch('gfeeds.rss_parser.ConfManager')
+    def test_Feed_create(self, ConfManager_mock, mock_file):
+        confman_mock = ConfManager_mock()
+        download_res = (
+            '/some/path',  # feedpath
+            'https://planet.gnome.org'  # feed link
+        )
+        # always have the one article selected, so alter the max article age
+        confman_mock.max_article_age = (
+            (
+                datetime.now(timezone.utc) -
+                datetime(2020, 3, 26, 15, 18, tzinfo=timezone.utc)
+            ) +
+            timedelta(days=100)
+        )
+        feed = Feed(download_res)
+
+        feed.is_null.should.be.false
+        feed.error.should.be.none
+        feed.rss_link.should.equal('https://planet.gnome.org')
+
+        # weird, testing a DOC, but feedparser is not a very stable DOC, so
+        # testing it on my side has some value
+        feed.fp_feed.should.be.a(dict)
+        feed.fp_feed.encoding.should.equal('utf-8')
+        feed.fp_feed.entries.should.have.length_of(1)
+
+        feed.title.should.equal('Planet GNOME')
+        feed.link.should.equal('https://planet.gnome.org/')
+        feed.description.should.equal(
+            'Planet GNOME - https://planet.gnome.org/'
+        )
+        feed.items.should.have.length_of(1)
+        # TODO: complete this test, there's more
